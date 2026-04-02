f062160655d6f7ea2b2874efb9adccf84df58302
amila-mirvic <amila12mirvic@gmail.com>
2026-04-02 22:20:09 +0200

    ui touch-ups

--- src/components/game/FlyItemsLayer.js
+++ src/components/game/FlyItemsLayer.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import styles from './FlyItemsLayer.module.css';
 
-export default function FlyItemsLayer({ items }) {
-  if (!items?.length) return null;
-  return items.map((item) => (
-    <div
-      key={item.id}
-      className={styles.wrap}
-      style={{
-        left: item.fromX,
-        top: item.fromY,
-        '--fromX': `${item.fromX}px`,
-        '--fromY': `${item.fromY}px`,
-        '--toX': `${item.toX}px`,
-        '--toY': `${item.toY}px`,
-      }}
-    >
-      <div className={styles.inner}>
-        <img src={item.icon} alt="" className={styles.icon} />
-        <div className={styles.delta}>{item.delta}</div>
-      </div>
+export default function FlyItemsLayer({ items = [] }) {
+  return (
+    <div className={styles.layer} aria-hidden="true">
+      {items.map((item) => {
+        const dx = item.toX - item.fromX;
+        const dy = item.toY - item.fromY;
+
+        return (
+          <div
+            key={item.id}
+            className={styles.flyItem}
+            style={{
+              left: `${item.fromX}px`,
+              top: `${item.fromY}px`,
+              '--dx': `${dx}px`,
+              '--dy': `${dy}px`,
+            }}
+          >
+            <img src={item.icon} alt="" className={styles.icon} />
+            {item.delta ? <span className={styles.delta}>{item.delta}</span> : null}
+          </div>
+        );
+      })}
     </div>
-  ));
+  );
 }
